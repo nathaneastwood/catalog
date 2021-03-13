536574bcfc861d6e33c15d2faabeb3c53a1c464d
nathaneastwood <nathan.eastwood@icloud.com>
2021-03-13 15:03:08 +0100

    refactor: Rename spark_catalog() -> catalog(), merge with invoke_catalog() file

--- R/catalog.R
+++ R/catalog.R
@@ -1,3 +1,18 @@
+#' Access the Metastore Management Interface
+#'
+#' @param sc A `spark_connection`.
+#'
+#' @return
+#' A `spark_jobj`.
+#'
+#' @importFrom sparklyr connection_is_open invoke spark_session
+#' @noRd
+catalog <- function(sc) {
+  stopifnot(sparklyr::connection_is_open(sc))
+  ss <- sparklyr::spark_session(sc)
+  sparklyr::invoke(ss, "catalog")
+}
+
 #' Invoke A Catalog Method
 #'
 #' @param sc A `spark_connection`.
@@ -8,6 +23,6 @@
 #' @noRd
 invoke_catalog <- function(sc, method, ...) {
   check_character_one(x = method)
-  catalog <- spark_catalog(sc = sc)
+  catalog <- catalog(sc = sc)
   sparklyr::invoke(jobj = catalog, method = method, ...)
 }
